0bd8cfdd2ae661a84d52be3ecc9948628c1a1a75
Kelvin Ndungu <kelvinnj103@gmail.com>
2025-12-01 22:43:04 +0300

    Fix syntax error and add CORS headers in vercel.json

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true });
+app.use(express.urlencoded({ extended: true }));
 
 // Request logging middleware
 app.use((req, res, next) => {

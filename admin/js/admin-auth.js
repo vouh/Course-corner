67d0adea75435a66adba1e5c41ce0b3e9553a509
Pvouh <kelvinkibirupeter1531@gmail.com>
2026-02-02 15:29:14 +0300

    feat: Implement admin panel with authentication, referrals, and withdrawals, removing payment status documentation and a debug script.

---
+++ admin/js/admin-auth.js
@@ -0,0 +1,56 @@
+/**
+ * Admin Authentication & Access Control
+ * Centralized list of authorized admin emails
+ */
+
+const ADMIN_EMAILS = [
+    'peterkelvinkibiru1532.com',
+    'admin@coursecornerkenya.com' // Example of adding another one
+];
+
+/**
+ * Checks if a user is authorized as an admin
+ * @param {firebase.User} user - The Firebase user object
+ * @returns {boolean} - True if authorized
+ */
+function isAuthorizedAdmin(user) {
+    if (!user || !user.email) return false;
+    return ADMIN_EMAILS.includes(user.email.toLowerCase());
+}
+
+/**
+ * Common auth state handler for admin pages
+ * Automatically redirects unauthorized users
+ * @param {firebase.User} user - Firebase user
+ * @param {Function} onSuccess - Callback when authorized
+ * @param {string} redirectUrl - Where to send unauthorized users
+ */
+function handleAdminAuthState(user, onSuccess, redirectUrl = 'index.html') {
+    if (user) {
+        if (isAuthorizedAdmin(user)) {
+            if (onSuccess) onSuccess(user);
+        } else {
+            console.error('Unauthorized admin access attempt:', user.email);
+            // Sign out and redirect
+            if (typeof firebase !== 'undefined') {
+                firebase.auth().signOut().then(() => {
+                    window.location.href = redirectUrl;
+                });
+            } else if (typeof auth !== 'undefined') {
+                auth.signOut().then(() => {
+                    window.location.href = redirectUrl;
+                });
+            } else {
+                window.location.href = redirectUrl;
+            }
+        }
+    } else {
+        // Only redirect if we are NOT on the index.html page (which contains the login)
+        const currentPage = window.location.pathname.split('/').pop();
+        if (currentPage && currentPage !== 'index.html') {
+            window.location.href = redirectUrl;
+        } else if (typeof showLogin === 'function') {
+            showLogin();
+        }
+    }
+}
